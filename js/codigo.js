180b4f3ba229ed5c81c4704ee43b6e3235259902
Alexis645-tech <castroalexis645@gmail.com>
2021-03-29 08:24:05 -0500

    Summit

---
+++ js/codigo.js
@@ -0,0 +1,9 @@
+function braille() {
+	document.querySelector(".Braille").style.display = "flex";
+	document.querySelector(".Señas").style.display = "none";
+}
+
+function lSenhas() {
+	document.querySelector(".Braille").style.display = "none";
+	document.querySelector(".Señas").style.display = "flex";
+}
